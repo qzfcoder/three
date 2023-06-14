c29e85d81df47a15d59be56db731b6c4d2385bad
qzfcoder <867303253@qq.com>
2023-06-14 09:21:58 +0800

    first commit

--- 01/src/main/11.双击控制全屏.js
+++ 01/src/main/11.双击控制全屏.js
@@ -6,9 +6,6 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import gsap from "gsap";
 // console.log(THREE);
 
-// 导入dat.gui库
-import * as dat from "dat.gui";
-
 // 创建一个场景
 const scene = new THREE.Scene();
 
@@ -26,24 +23,24 @@ scene.add(camera);
 
 // 添加物体
 // 创建几何体对象
-const Geometry = new THREE.BufferGeometry();
-
-const vertices = new Float32Array([
-  -1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1,
-]);
-
-Geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
-
-// 更具几何体额材质创建物体
-
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 // 材质
-const Material = new THREE.MeshBasicMaterial({
+const cubeMaterial = new THREE.MeshBasicMaterial({
   color: 0xffff,
 });
 // 根据集合体和材质创建物体
-const mesh = new THREE.Mesh(Geometry, Material);
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-scene.add(mesh);
+// 修改物体的位置
+cube.position.set(1, 0, 0);
+// 设置缩放位置
+cube.scale.set(3, 2, 1);
+// 旋转
+cube.rotation.set(Math.PI / 4, Math.PI / 2, Math.PI / 2);
+
+// 添加到场景中
+scene.add(cube);
+
 // 渲染出啦
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -64,6 +61,24 @@ controls.enableDamping = true;
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
+// 设置时钟
+const clock = new THREE.Clock();
+// 设置动画
+var animate1 = gsap.to(cube.position, {
+  x: 5,
+  duration: 5,
+  repeat: -1, // -1 无限次循环
+  yoyo: true, // 往返运动
+  delay: 2, // 延迟2s运动
+  ease: "power1.inOut",
+  onComplete: () => {
+    console.log("动画完成--position");
+  },
+  onStart: () => {
+    console.log("动画开始--position");
+  },
+});
+gsap.to(cube.rotation, { x: Math.PI, duration: 5, ease: "power1.inOut" });
 // 控制全屏控制
 window.addEventListener("dblclick", () => {
   const fullScreenElement = document.fullscreenElement;
@@ -74,10 +89,27 @@ window.addEventListener("dblclick", () => {
     console.log(document);
     document.exitFullscreen();
   }
+
+  // if (animate1.isActive()) {
+  //   animate1.pause();
+  // } else {
+  //   animate1.resume();
+  // }
 });
 
 function render() {
   controls.update();
+  // // 获取时钟运行总时长
+  // let time = clock.getElapsedTime();
+  // // let deltaTime = clock.getDelta();
+  // // console.log("时钟运行的总时长：", time);
+  // // console.log("两次获取时间的间隔：", deltaTime);
+  // let t = time % 5;
+  // cube.position.x = t * 1;
+  // cube.rotation.x = t * 1;
+  // if (cube.position.x > 5) {
+  //   cube.position.x = 0;
+  // }
   renderer.render(scene, camera);
   // 渲染下一帧的时候
   requestAnimationFrame(render);
--- 01/src/main/12.gui控制scane.js
+++ 01/src/main/12.gui控制scane.js
@@ -26,24 +26,73 @@ scene.add(camera);
 
 // 添加物体
 // 创建几何体对象
-const Geometry = new THREE.BufferGeometry();
-
-const vertices = new Float32Array([
-  -1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1,
-]);
-
-Geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
-
-// 更具几何体额材质创建物体
-
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 // 材质
-const Material = new THREE.MeshBasicMaterial({
+const cubeMaterial = new THREE.MeshBasicMaterial({
   color: 0xffff,
 });
 // 根据集合体和材质创建物体
-const mesh = new THREE.Mesh(Geometry, Material);
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-scene.add(mesh);
+// 修改物体的位置
+cube.position.set(1, 0, 0);
+// 设置缩放位置
+cube.scale.set(3, 2, 1);
+// 旋转
+cube.rotation.set(Math.PI / 4, Math.PI / 2, Math.PI / 2);
+
+const gui = new dat.GUI();
+gui
+  .add(cube.position, "y")
+  .min(0)
+  .max(5)
+  .name("11")
+  .onChange((e) => {
+    console.log("修改的值", e);
+  })
+  .onFinishChange((e) => {
+    console.log("结束", e);
+  });
+// 修改物体的颜色
+const params = {
+  color: "#ffff00",
+  fn: () => {
+    let animation;
+    if (animation) {
+      animation = null;
+    } else {
+      // 让立方图运动起来
+      animation = gsap.to(cube.position, {
+        x: 5,
+        duration: 5,
+        repeat: -1, // -1 无限次循环
+        yoyo: true, // 往返运动
+        // delay: 2, // 延迟2s运动
+        ease: "power1.inOut",
+        onComplete: () => {
+          console.log("动画完成--position");
+        },
+        onStart: () => {
+          console.log("动画开始--position");
+        },
+      });
+    }
+  },
+};
+gui.addColor(params, "color").onChange((e) => {
+  console.log("e", e);
+  cube.material.color.set(e);
+});
+// 控制显示隐藏
+gui.add(cube, "visible").name("显示隐藏");
+// 点击触发摸个事件
+gui.add(params, "fn").name("点击");
+
+let folder = gui.addFolder("设置立方体");
+folder.add(cube.material, "wireframe").name("是否显示线框");
+// 添加到场景中
+scene.add(cube);
+
 // 渲染出啦
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -64,6 +113,9 @@ controls.enableDamping = true;
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
+// 设置动画
+// var animate1 =
+gsap.to(cube.rotation, { x: Math.PI, duration: 5, ease: "power1.inOut" });
 // 控制全屏控制
 window.addEventListener("dblclick", () => {
   const fullScreenElement = document.fullscreenElement;
